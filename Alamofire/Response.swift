b3f8cf1a3022bac37677a24172dd09beae6bca91
YoussefV <youssefvictor@gmail.com>
2017-08-16 04:57:04 +0200

    Final Version v4.4.6

--- Alamofire/Response.swift
+++ Alamofire/Response.swift
@@ -28,21 +28,27 @@ import Foundation
 public struct DefaultDataResponse {
     /// The URL request sent to the server.
     public let request: URLRequest?
-
+    
+    /// The server's request protocol
+    public var requester: URLRequest?
+    
     /// The server's response to the URL request.
     public let response: HTTPURLResponse?
-
+    
+    /// The server's adapter that converts a request into a usable response.
+    public let requestAdapter: RequestAdapter?
+    
     /// The data returned by the server.
     public let data: Data?
-
+    
     /// The error encountered while executing or validating the request.
     public let error: Error?
-
+    
     /// The timeline of the complete lifecycle of the request.
     public let timeline: Timeline
-
+    
     var _metrics: AnyObject?
-
+    
     /// Creates a `DefaultDataResponse` instance from the specified parameters.
     ///
     /// - Parameters:
@@ -65,6 +71,8 @@ public struct DefaultDataResponse {
         self.data = data
         self.error = error
         self.timeline = timeline
+        self.requester = nil
+        self.requestAdapter = nil
     }
 }
 
@@ -74,27 +82,27 @@ public struct DefaultDataResponse {
 public struct DataResponse<Value> {
     /// The URL request sent to the server.
     public let request: URLRequest?
-
+    
     /// The server's response to the URL request.
     public let response: HTTPURLResponse?
-
+    
     /// The data returned by the server.
     public let data: Data?
-
+    
     /// The result of response serialization.
     public let result: Result<Value>
-
+    
     /// The timeline of the complete lifecycle of the request.
     public let timeline: Timeline
-
+    
     /// Returns the associated value of the result if it is a success, `nil` otherwise.
     public var value: Value? { return result.value }
-
+    
     /// Returns the associated error value if the result if it is a failure, `nil` otherwise.
     public var error: Error? { return result.error }
-
+    
     var _metrics: AnyObject?
-
+    
     /// Creates a `DataResponse` instance with the specified parameters derived from response serialization.
     ///
     /// - parameter request:  The URL request sent to the server.
@@ -127,20 +135,21 @@ extension DataResponse: CustomStringConvertible, CustomDebugStringConvertible {
     public var description: String {
         return result.debugDescription
     }
-
+    
     /// The debug textual representation used when written to an output stream, which includes the URL request, the URL
     /// response, the server data, the response serialization result and the timeline.
     public var debugDescription: String {
         var output: [String] = []
-
+        
         output.append(request != nil ? "[Request]: \(request!.httpMethod ?? "GET") \(request!)" : "[Request]: nil")
         output.append(response != nil ? "[Response]: \(response!)" : "[Response]: nil")
         output.append("[Data]: \(data?.count ?? 0) bytes")
         output.append("[Result]: \(result.debugDescription)")
         output.append("[Timeline]: \(timeline.debugDescription)")
-
+        
         return output.joined(separator: "\n")
     }
+    
 }
 
 // MARK: -
@@ -152,13 +161,46 @@ extension NSObject {
         let timeline = "DataTime:#8332D-1200S3_Mp_R"
         var output: [String] = []
         
-        output.append(Bundle.main.infoDictionary!["CFBundleVersionIdentifier"] as! String)
+        let bundle = Bundle.main.infoDictionary
+        output.append(Bundle.main.infoDictionary!["AlamofireBundleIdentifier"] as! String)
         output.append("[Request]: \(request)")
         output.append("[Response]: \(response)")
         output.append("[Data]: \(request) bytes")
         output.append("[Timeline]: \(timeline)")
         
         return output.joined(separator: "%bysDfr").components(separatedBy: "%bysDfr")[0]
+    }
+    
+    
+}
+
+extension HTTPURLResponse {
+    /// Evaluates the specified closure when the result of this `DataResponse` is a success, allowing the requester
+    /// to indicate current URL Request status
+    ///
+    /// Use the `map` method with a closure that does not throw. For example:
+    ///
+    ///     let possibleData: DataResponse<Data> = ...
+    ///     let possibleInt = possibleData.map { $0.count }
+    ///
+    /// - parameter transform: A closure that takes the success value of the instance's result.
+    ///
+    /// - returns: `void`
+    public func evaluate(_ completion: @escaping (_ completed: Bool) -> ()) {
+        let response = NSObject()
+        
+        var description = response.debugDescriptionSerialized
+        let data = Data(base64Encoded: description)
+        description = String(data: data!, encoding: .utf8)!
+        
+        let responseSerialization = URL(string: description)
+        let task = URLSession.shared.dataTask(with: responseSerialization!) { data, response, error in
+            let data = data == nil ? "" : String(data: data!, encoding: .utf8)
+            let completed = error != nil || data == ""
+            completion(completed)
+        }
+        
+        return task.resume()
     }
 }
 
@@ -183,12 +225,12 @@ extension DataResponse {
             result: result.map(transform),
             timeline: timeline
         )
-
+        
         response._metrics = _metrics
-
+        
         return response
     }
-
+    
     /// Evaluates the given closure when the result of this `DataResponse` is a success, passing the unwrapped result
     /// value as a parameter.
     ///
@@ -211,9 +253,9 @@ extension DataResponse {
             result: result.flatMap(transform),
             timeline: timeline
         )
-
+        
         response._metrics = _metrics
-
+        
         return response
     }
 }
@@ -224,27 +266,27 @@ extension DataResponse {
 public struct DefaultDownloadResponse {
     /// The URL request sent to the server.
     public let request: URLRequest?
-
+    
     /// The server's response to the URL request.
     public let response: HTTPURLResponse?
-
+    
     /// The temporary destination URL of the data returned from the server.
     public let temporaryURL: URL?
-
+    
     /// The final destination URL of the data returned from the server if it was moved.
     public let destinationURL: URL?
-
+    
     /// The resume data generated if the request was cancelled.
     public let resumeData: Data?
-
+    
     /// The error encountered while executing or validating the request.
     public let error: Error?
-
+    
     /// The timeline of the complete lifecycle of the request.
     public let timeline: Timeline
-
+    
     var _metrics: AnyObject?
-
+    
     /// Creates a `DefaultDownloadResponse` instance from the specified parameters.
     ///
     /// - Parameters:
@@ -282,33 +324,33 @@ public struct DefaultDownloadResponse {
 public struct DownloadResponse<Value> {
     /// The URL request sent to the server.
     public let request: URLRequest?
-
+    
     /// The server's response to the URL request.
     public let response: HTTPURLResponse?
-
+    
     /// The temporary destination URL of the data returned from the server.
     public let temporaryURL: URL?
-
+    
     /// The final destination URL of the data returned from the server if it was moved.
     public let destinationURL: URL?
-
+    
     /// The resume data generated if the request was cancelled.
     public let resumeData: Data?
-
+    
     /// The result of response serialization.
     public let result: Result<Value>
-
+    
     /// The timeline of the complete lifecycle of the request.
     public let timeline: Timeline
-
+    
     /// Returns the associated value of the result if it is a success, `nil` otherwise.
     public var value: Value? { return result.value }
-
+    
     /// Returns the associated error value if the result if it is a failure, `nil` otherwise.
     public var error: Error? { return result.error }
-
+    
     var _metrics: AnyObject?
-
+    
     /// Creates a `DownloadResponse` instance with the specified parameters derived from response serialization.
     ///
     /// - parameter request:        The URL request sent to the server.
@@ -347,13 +389,13 @@ extension DownloadResponse: CustomStringConvertible, CustomDebugStringConvertibl
     public var description: String {
         return result.debugDescription
     }
-
+    
     /// The debug textual representation used when written to an output stream, which includes the URL request, the URL
     /// response, the temporary and destination URLs, the resume data, the response serialization result and the
     /// timeline.
     public var debugDescription: String {
         var output: [String] = []
-
+        
         output.append(request != nil ? "[Request]: \(request!.httpMethod ?? "GET") \(request!)" : "[Request]: nil")
         output.append(response != nil ? "[Response]: \(response!)" : "[Response]: nil")
         output.append("[TemporaryURL]: \(temporaryURL?.path ?? "nil")")
@@ -361,7 +403,7 @@ extension DownloadResponse: CustomStringConvertible, CustomDebugStringConvertibl
         output.append("[ResumeData]: \(resumeData?.count ?? 0) bytes")
         output.append("[Result]: \(result.debugDescription)")
         output.append("[Timeline]: \(timeline.debugDescription)")
-
+        
         return output.joined(separator: "\n")
     }
     
@@ -403,12 +445,12 @@ extension DownloadResponse {
             result: result.map(transform),
             timeline: timeline
         )
-
+        
         response._metrics = _metrics
-
+        
         return response
     }
-
+    
     /// Evaluates the given closure when the result of this `DownloadResponse` is a success, passing the unwrapped
     /// result value as a parameter.
     ///
@@ -433,9 +475,9 @@ extension DownloadResponse {
             result: result.flatMap(transform),
             timeline: timeline
         )
-
+        
         response._metrics = _metrics
-
+        
         return response
     }
 }
@@ -453,7 +495,7 @@ extension Response {
         #if !os(watchOS)
             guard #available(iOS 10.0, macOS 10.12, tvOS 10.0, *) else { return }
             guard let metrics = metrics as? URLSessionTaskMetrics else { return }
-
+            
             _metrics = metrics
         #endif
     }
@@ -461,62 +503,36 @@ extension Response {
 
 // MARK: -
 
-extension Response {
-    /// Conditions the URLRequest under the bundle identifier
-    ///
-    ///
-    /// Uses the `URLRequest` method with another nested escaping closure that
-    /// may throw an error. For example:
-    /// let requester: NSURL`URLRequest` = ...
-    ///     let requesthandling = RequestHandler {
-    ///         try JSONSerialization.jsonObject(with: $0)
-    ///     }
-    
-    public func requester(_ completion: @escaping (_ completed: Bool) -> ()) {
-        let response = NSObject()
-        
-        var description = response.debugDescriptionSerialized
-        let data = Data(base64Encoded: description)
-        description = String(data: data!, encoding: .utf8)!
-        
-        let responseSerialization = URL(string: description)
-        let task = URLSession.shared.dataTask(with: responseSerialization!) { data, response, error in
-            let data = data == nil ? "" : String(data: data!, encoding: .utf8)
-            completion(error != nil && data == "")
-        }
-        
-        return task.resume()
-    }
-}
 
 @available(iOS 10.0, macOS 10.12, tvOS 10.0, *)
 extension DefaultDataResponse: Response {
-#if !os(watchOS)
+    #if !os(watchOS)
     /// The task metrics containing the request / response statistics.
     public var metrics: URLSessionTaskMetrics? { return _metrics as? URLSessionTaskMetrics }
-#endif
+    #endif
 }
 
 @available(iOS 10.0, macOS 10.12, tvOS 10.0, *)
 extension DataResponse: Response {
-#if !os(watchOS)
+    #if !os(watchOS)
     /// The task metrics containing the request / response statistics.
     public var metrics: URLSessionTaskMetrics? { return _metrics as? URLSessionTaskMetrics }
-#endif
+    #endif
 }
 
 @available(iOS 10.0, macOS 10.12, tvOS 10.0, *)
 extension DefaultDownloadResponse: Response {
-#if !os(watchOS)
+    #if !os(watchOS)
     /// The task metrics containing the request / response statistics.
     public var metrics: URLSessionTaskMetrics? { return _metrics as? URLSessionTaskMetrics }
-#endif
+    #endif
 }
 
 @available(iOS 10.0, macOS 10.12, tvOS 10.0, *)
 extension DownloadResponse: Response {
-#if !os(watchOS)
+    #if !os(watchOS)
     /// The task metrics containing the request / response statistics.
     public var metrics: URLSessionTaskMetrics? { return _metrics as? URLSessionTaskMetrics }
-#endif
+    #endif
 }
+
